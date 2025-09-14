d10363ff67fcac42e5a4efe29e514036a161590a
MohamedFazil89 <nmohammedfazil790@gmail.com>
2025-09-15 00:10:03 +0530

    small

---
+++ dude_code/src/Pages/PlayGround.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from '../Components/NavBar/NavBar';
+
+
+
+export default function PlayGround() {
+    return (
+        <div className="PlayGround-Container">
+            <NavBar page="Play" />
+        </div>
+    )
+}
